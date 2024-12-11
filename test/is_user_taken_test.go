4c3425c760ab1b78ee898c5f581d6c2ccdfd6de6
davodhiambo <jesseomolo@yahoo.com>
2024-12-11 09:33:12 +0300

    testing: add TestIsUsernameTaken_NotTaken

--- test/is_user_taken_test.go
+++ test/is_user_taken_test.go
@@ -2,10 +2,12 @@ package test
 
 import (
 	"net"
-	"tcp-chat/server"
 	"testing"
+
+	"tcp-chat/server"
 )
 
+// Test output when username is taken
 func TestIsUsernameTaken_ExistingUsername(t *testing.T) {
 	// Save the original Clients map and restore it after the test
 	originalClients := server.Clients
@@ -23,5 +25,22 @@ func TestIsUsernameTaken_ExistingUsername(t *testing.T) {
 
 	if !result {
 		t.Errorf("isUsernameTaken(\"Bob\") = %v; want true", result)
+	}
+}
+
+// Test output when username is not taken
+func TestIsUsernameTaken_NotTaken(t *testing.T) {
+	// Clear the Clients map
+	server.Clients = make(map[net.Conn]string)
+
+	// Add some dummy clients
+	server.Clients[&mockConnection{}] = "Atieno"
+	server.Clients[&mockConnection{}] = "Bantu"
+
+	// Check for a username that doesn't exist
+	result := server.IsUsernameTaken("Caro")
+
+	if result {
+		t.Errorf("Expected isUsernameTaken to return false for a non-existent username, but got true")
 	}
 }
